47e78c4d9d214b0f9dafb0028bfe9485f966b9cb
NTiger07 <falaleru@gmail.com>
2023-03-06 13:06:38 +0100

    added datatypes

--- Main.java
+++ Main.java
@@ -3,6 +3,28 @@ class Main {
         int num1 = 15;
         int num2 = 20;
 
-        System.out.println(num1 + num2);
+        System.out.println("We are " + num1 + num2 + " years old");
+        dataTypes(null);
+    }
+
+    public static void dataTypes(String[] args) {
+        // Integer types
+        byte aSingleByte = 100;
+        short aSmallNumber = 20000;
+        int anInteger = 2147454656;
+        long aLargeNumber = 7757586773473434434L;
+
+        // Decimal types
+        double aDouble = 1.79769313;
+        float aFloat = 3.4028F;
+
+        // Booleans
+        boolean isMessiTheGOAT = true;
+        boolean isJavaEasy = false;
+
+        // Characters
+        char copyrightSymbol = '\u00A9';
+
+        System.out.print("This is the copyright symbol " + copyrightSymbol);
     }
 }
