ec99ec768bc1b88c579656d3e27522b01ad84c29
NTiger07 <falaleru@gmail.com>
2023-03-06 23:54:12 +0100

    added string methods

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
 
         // System.out.println("We are " + num1 + num2 + " years old");
         // dataTypes(null);
-        StringFormating();
+        StringFormatingYMethods();
     }
 
     public static void dataTypes(String[] args) {
@@ -32,14 +32,19 @@ class Main {
         System.out.print("This is the copyright symbol " + copyrightSymbol);
     }
 
-    public static void StringFormating() {
-        String myName = "Favour";
-        int age = 69;
+    public static void StringFormatingYMethods() {
+        String myName = "Favour"; // %s
+        int age = 69; // %d
         String country = "Nigeria";
-
         String formattedString = String.format("My name is %s. I am from %s. I am %d years old.", myName, country, age);
-
         System.out.print(formattedString);
+
+        // String methods
+        String fullName = "Olaleru Favour";
+
+        System.out.println(fullName.length());
+        System.out.println(fullName.toUpperCase());
+        System.out.println(fullName.toLowerCase());
 
     }
 }
