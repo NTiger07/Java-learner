e3769264ee4bdc1df337f8e36909d633fd09a11f
NTiger07 <falaleru@gmail.com>
2023-03-07 16:42:00 +0100

    userinput done

--- Main.java
+++ Main.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        int num1 = 15;
-        int num2 = 20;
-
-        // System.out.println("We are " + num1 + num2 + " years old");
         // dataTypes(null);
-        StringFormatingYMethods();
+        // StringFormatingYMethods();
+        UserInput();
     }
 
     public static void dataTypes(String[] args) {
@@ -53,6 +50,19 @@ class Main {
     }
 
     public static void UserInput() {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? : ");
+        String name = scanner.nextLine();
+
+        System.out.printf("Hello %s! How old are you? : ", name);
+        int age = scanner.nextInt();
+        System.out.printf("%d is an excellent age to start learning Java!", age);
+
+        // scanner.nextDouble();
+        // scanner.nextBoolean();
+        // scanner.nextFloat();
+
+        scanner.close();
+
     }
 }
