59b0b1cac9296d16956c7b0b9243049dd5cd930a
NTiger07 <falaleru@gmail.com>
2023-03-08 19:24:00 +0100

    for loops

--- Main.java
+++ Main.java
@@ -7,7 +7,9 @@ class Main {
         // StringFormatingYMethods();
         // UserInput();
         // Conditionals();
-        Arrays();
+        // Arrays();
+        // ForLoops();
+        WhileLoops();
     }
 
     public static void dataTypes(String[] args) {
@@ -143,9 +145,35 @@ class Main {
 
         // Copying an Array
         int numbers[] = { 1, 2, 3, 4, 5 };
-
         // int copyOfNumbers[] = numbers; // reference type
         int copyOfNumbers[] = Arrays.copyOf(numbers, numbers.length); // non reference type
+    }
+
+    public static void ForLoops() {
+        int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Sum of numbers in number Aray
+        int sum = 0;
+
+        for (int index = 0; index < numbers.length; index++) {
+            sum += numbers[index];
+        }
+        System.out.println(sum);
+
+        // Nested for loop to print multiplication table
+        for (int number = 1; number <= 10; number++) {
+            for (int multiplier = 1; multiplier <= 10; multiplier++) {
+                System.out.printf("%d X %d = %d ", number, multiplier, number * multiplier);
+            }
+            System.out.println("\n");
+        }
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    public static void WhileLoops() {
 
     }
 }
