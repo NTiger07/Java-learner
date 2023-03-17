716ad182a3face1f831453b1f6e64fe4dac0e334
NTiger07 <falaleru@gmail.com>
2023-03-17 17:49:58 +0100

    added 2d lists

---
+++ BroCode/TwoDimensionLists.java
@@ -0,0 +1,34 @@
+package BroCode;
+
+import java.util.*;
+
+public class TwoDimensionLists {
+
+    public static void twoDimensionLists(String[] args) {
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+
+        ArrayList<String> drinksList = new ArrayList<String>();
+
+        drinksList.add("Fanta");
+        drinksList.add("Sprite");
+        drinksList.add("Hollandia");
+
+        ArrayList<String> produceList = new ArrayList<String>();
+
+        produceList.add("Tomato");
+        produceList.add("Pepper");
+        produceList.add("Garlic");
+
+        ArrayList<String> pastryList = new ArrayList<String>();
+
+        pastryList.add("Donut");
+        pastryList.add("Meatpie");
+        pastryList.add("Fishroll");
+
+        groceryList.add(drinksList);
+        groceryList.add(produceList);
+        groceryList.add(pastryList);
+
+        System.out.println(groceryList);
+    }
+}
