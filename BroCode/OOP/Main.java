89003fbefe1d2a0f1001bf40536e24411af27a4d
NTiger07 <falaleru@gmail.com>
2023-03-18 14:36:00 +0100

    garage and car classes work

--- BroCode/OOP/Main.java
+++ BroCode/OOP/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Garage garage1 = new Garage(1, 20, 20);
 
         Car car1 = new Car("Toyota", "Camry", 2022, "Coupe", "Red");
+        Car car2 = new Car("Hyundai", "Sonata", 2022, "Coupe", "Black");
 
-        // System.out.println(garage1.getRemainingCapacity());
+        System.out.println(garage1.getRemainingCapacity());
         System.out.println(garage1.park(car1));
-        // System.out.println(garage1.getRemainingCapacity());
+        System.out.println(garage1.park(car2));
+        System.out.println(garage1.getRemainingCapacity());
         // System.out.println(car1);
     }
 
