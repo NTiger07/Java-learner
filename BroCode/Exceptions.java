cee16e91bf4446724b50a0e3822ad1ffbdc8dfb4
NTiger07 <falaleru@gmail.com>
2023-03-19 23:53:21 +0100

    file class

---
+++ BroCode/Exceptions.java
@@ -0,0 +1,30 @@
+package BroCode;
+
+import java.util.*;
+
+public class Exceptions {
+    public static void exception(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a whole number: ");
+        int x = scanner.nextInt();
+
+        System.out.print("Enter another whole number to divide by: ");
+        int y = scanner.nextInt();
+
+        try {
+            System.out.println(String.format("Result: %d", x / y));
+        } catch (ArithmeticException e) {
+            System.out.println("You can't divide by zero!!");
+        } catch (InputMismatchException e) {
+            scanner.nextLine();
+            System.out.println("Please enter an integer");
+        } catch (Exception e) {
+            System.out.println("An error occurred");
+        }
+        System.out.println("This will always run");
+        scanner.close();
+
+    }
+
+}
--- BroCode/Main.java
+++ BroCode/Main.java
@@ -1,30 +1,14 @@
 package BroCode;
 
-import java.util.*;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        File file1 = new File("./BroCode/timestamps.txt"); // returns true, "timestamps.txt returns false"
 
-        System.out.print("Enter a whole number: ");
-        int x = scanner.nextInt();
+        // file1.delete();
 
-        System.out.print("Enter another whole number to divide by: ");
-        int y = scanner.nextInt();
-
-        try {
-            System.out.println(String.format("Result: %d", x / y));
-        } catch (ArithmeticException e) {
-            System.out.println("You can't divide by zero!!");
-        } catch (InputMismatchException e) {
-            scanner.nextLine();
-            System.out.println("Please enter an integer");
-        } catch (Exception e) {
-            System.out.println("An error occurred");
-        }
-        System.out.println("This will always run");
-        scanner.close();
-
+        System.out.println(file1.exists());
     }
 
 }
