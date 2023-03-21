75c925aa7852cc7562a398babc516471c9150bec
NTiger07 <falaleru@gmail.com>
2023-03-21 18:37:47 +0100

    fixed user class

--- BroCode/Serializer/User.java
+++ BroCode/Serializer/User.java
@@ -3,12 +3,20 @@ package BroCode.Serializer;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    String name;
-    String password;
+    private String name;
+    private String password;
 
     User(String name, String password) {
         this.name = name;
         this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
     }
 
     @Override
