e92dce17cf6a063a1bceab87cde8f7ab47aed871
NTiger07 <falaleru@gmail.com>
2023-03-21 19:00:53 +0100

    timers

--- BroCode/Main.java
+++ BroCode/Main.java
@@ -1,7 +1,29 @@
 package BroCode;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            int counter = 10;
+
+            @Override
+            public void run() {
+                if (counter > 0) {
+                    System.out.printf("%d seconds remaining \n", counter);
+                    counter--;
+                } else {
+                    System.out.println("Happy new year!!!");
+                    timer.cancel();
+                }
+            }
+
+        };
+
+        // timer.schedule(task, 2000);
+        timer.scheduleAtFixedRate(task, 0, 1000);
 
     }
 
