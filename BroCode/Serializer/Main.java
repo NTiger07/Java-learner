1485dc094272713c37be309b665b936414e80537
NTiger07 <falaleru@gmail.com>
2023-03-21 15:47:54 +0100

    added steps to serialize

--- BroCode/Serializer/Main.java
+++ BroCode/Serializer/Main.java
@@ -1,7 +1,14 @@
 package BroCode.Serializer;
 
 public class Main {
-    public static void main() {
+    // Steps to Serialize
+    // Object class should implement Serializable interface
+    // FileOutputStream fileout = new FileOutputStream(file_path)
+    // ObjectOutputStream out = new ObjectOutputStream(fileout)
+    // out.writeObject(object_name)
+    // out.close(); fileOut.close();
+
+    public static void main(String[] args) {
 
         User user1 = new User("Beckham", "davidbeckham724");
 
--- BroCode/Serializer/User.java
+++ BroCode/Serializer/User.java
@@ -1,6 +1,8 @@
 package BroCode.Serializer;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String name;
     private String password;
 
