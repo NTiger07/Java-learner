2957e29fe23f304848fb83d605c007d6f1202c57
NTiger07 <falaleru@gmail.com>
2023-03-21 15:34:54 +0100

    added serializer folder

---
+++ BroCode/Serializer/Main.java
@@ -0,0 +1,13 @@
+package BroCode.Serializer;
+
+public class Main {
+
+    public static void main() {
+
+        User user1 = new User("Beckham", "davidbeckham724");
+
+        System.out.println(user1.getName());
+        System.out.println(user1.getPassword());
+    }
+
+}
---
+++ BroCode/Serializer/User.java
@@ -0,0 +1,19 @@
+package BroCode.Serializer;
+
+public class User {
+    private String name;
+    private String password;
+
+    User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
